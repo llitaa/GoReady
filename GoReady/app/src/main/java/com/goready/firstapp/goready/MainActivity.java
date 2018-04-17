f6931ce7b29aeb88ce322e7f4f36515e64681d43
llitaa <antonlytvynenko@gmail.com>
2018-04-17 17:05:17 +0200

    minor change

--- GoReady/app/src/main/java/com/goready/firstapp/goready/MainActivity.java
+++ GoReady/app/src/main/java/com/goready/firstapp/goready/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     protected void testChange() {
-        System.out.print("Test change");
+        System.out.print("Test small change");
     }
 }
